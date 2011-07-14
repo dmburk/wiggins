a1c5a6355a7d2a15e516902008f0cda88dab37a3
Michael Burk <michael.burk@gmail.com>
2011-07-14 14:11:39 -0400

    Initial commit. Have already done some things.

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'sqlite3'
+gem 'haml-rails'
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'rspec'
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'webrat'
+  gem 'database_cleaner'
+  gem 'jquery-rails'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'escape_utils'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wiggins::Application.config.secret_token = 'da143bc051421d5456cfa02a51606ae586e904536dc4ea240890552e69240bfbb4e079d026a15658dea07f98cf82a99b224844975bd75220874c44b55884e600'
