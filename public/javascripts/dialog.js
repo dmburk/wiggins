0d36993b6d994867570e9c9eda386fc8bd8c1183
Michael Burk <michael@iceman.(none)>
2011-10-18 12:27:32 -0400

    Added CSS styling to customer index and show pages.

---
+++ public/javascripts/dialog.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  var $customer_form = $('#customer_form').dialog({
+    autoOpen: false,
+    title: 'New Customer',
+    modal: true,
+    draggable: false
+  });
+});
