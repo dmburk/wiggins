9c9c842c5509343cdf201881cd95b3009e5da47d
Michael Burk <michael.burk@gmail.com>
2011-10-21 22:34:39 -0400

    Removed deleted files.

--- public/javascripts/dialog.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  var $customer_form = $('#customer_form').dialog({
-    autoOpen: false,
-    title: 'New Customer',
-    modal: true,
-    draggable: false
-  });
-});
